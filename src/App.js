6b70756d9ece4bc28c6308db8f682e48abcb6a17
unknown <kesupakayashwnth999@gmail.com>
2025-08-04 18:36:55 +0530

    night mode done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Dashboard from './Components/Dashboard'
 import Timeframe from './Components/Timeframe'
 import Stickynavbar from './Components/Stickynavbar'
 import Example from './Components/Example'
-import Examples from './Components/Examples'
 
 
 function App() {
